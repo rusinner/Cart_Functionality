0fb3e218e6f908bab94a6f3d7e2d802864db44e6
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-19 16:34:02 +0200

    start the project

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
 import "./App.css";
 
 function App() {
